ecb1bf30a7ee0e13706effa54e30dc0c08610b51
Abolfaz <nikfarabolfaz@gmail.com>
2023-01-02 12:50:50 +0330

    new edite

--- cornometer.js
+++ cornometer.js
@@ -3,98 +3,107 @@ var cornometer = /** @class */ (function () {
     function cornometer() {
         this._duration = 0;
         this._status = "stopped";
+        this.render();
     }
     cornometer.prototype.start = function () {
         if (this._status == "started")
             throw new Error("The cornometer already started");
-        this._currentTime = Date.now();
-        this._status = "started";
+        else {
+            this._currentTime = Date.now();
+            this._status = "started";
+        }
     };
     cornometer.prototype.stop = function () {
         if (this._status == "stopped")
             throw new Error("The cornometer already stopped");
-        //this._duration+=1;
-        this._duration = this._currentTime + this._duration;
-        this.show();
-        this._status = "stopped";
-    };
-    // public checkStatus() : string {
-    //     if (this._status == "started")
-    //     {
-    //         this._duration +=1; //= this._duration + this._currentTime;
-    //         //this._currentTime = Date.now();
-    //     }
-    //     return this.show();
-    // }
-    cornometer.prototype.show = function () {
-        var minute, second, milisecond, tempDuration;
-        tempDuration = this._duration;
-        minute = Math.floor(tempDuration / 60000);
-        tempDuration %= 60000;
-        second = Math.floor(tempDuration / 1000);
-        milisecond = tempDuration % 1000;
-        var finalMinute, finalSecond, finalMilisecond;
-        if (Math.floor(minute / 10) == 0) {
-            finalMinute = "0";
-            finalMinute += minute.toString();
+        else {
+            clearInterval(this._intervalID);
+            this._duration = (Date.now() - this._currentTime) + this._duration;
+            this._status = "stopped";
         }
-        else
-            finalMinute = minute.toString();
-        if (Math.floor(second / 10) == 0) {
-            finalSecond = "0";
-            finalSecond += second.toString();
-        }
-        else
-            finalSecond = second.toString();
-        finalMilisecond = milisecond.toString();
-        var finalDuration = finalMinute + ":" + finalSecond + "." + finalMilisecond;
-        console.log(finalDuration);
-        //return finalDuration;
     };
     cornometer.prototype.reset = function () {
         if (this._status == "started")
             this.stop();
         this._duration = 0;
     };
+    cornometer.prototype.render = function () {
+        var _this = this;
+        var divAnyCornometer = document.createElement("div");
+        divAnyCornometer.style.border = "2px solid black";
+        divAnyCornometer.style.width = "300px";
+        divAnyCornometer.style.height = "100px";
+        //////////////////////////////////////////////////////////////////////
+        var divShowTime = document.createElement("div");
+        divShowTime.style.border = "1px solid black";
+        divShowTime.style.width = "200px";
+        divShowTime.style.height = "50px";
+        divShowTime.style.margin = "10px";
+        //divShowTime.setAttribute("id", `showTime${index}`);
+        /////////////////////////////////////////////////////////////////////
+        var startBtn = document.createElement("button");
+        startBtn.innerText = "Start";
+        startBtn.addEventListener("click", function () {
+            _this.start();
+            _this._intervalID = setInterval(function () {
+                divShowTime.innerHTML = formatTime((Date.now() - _this._currentTime) + _this._duration);
+            }, 10);
+        });
+        var stopBtn = document.createElement("button");
+        stopBtn.innerText = "Stop";
+        stopBtn.addEventListener("click", function () {
+            _this.stop();
+            //clearInterval(time);
+            //time = 0;
+        });
+        var resetBtn = document.createElement("button");
+        resetBtn.innerText = "Reset";
+        resetBtn.addEventListener("click", function () {
+            _this.reset();
+            divShowTime.innerHTML = formatTime(_this._duration);
+        });
+        /////////////////////////////////////////////////////////////////////
+        divAnyCornometer.appendChild(divShowTime);
+        divAnyCornometer.appendChild(startBtn);
+        divAnyCornometer.appendChild(stopBtn);
+        divAnyCornometer.appendChild(resetBtn);
+        document.body.appendChild(divAnyCornometer);
+    };
     return cornometer;
 }());
-function creatCornometerDOM(list, index) {
-    var divAnyCornometer = document.createElement("div");
-    divAnyCornometer.style.border = "2px solid black";
-    divAnyCornometer.style.width = "300px";
-    divAnyCornometer.style.height = "100px";
-    //////////////////////////////////////////////////////////////////////
-    var divShowTime = document.createElement("div");
-    divShowTime.style.border = "1px solid black";
-    divShowTime.style.width = "200px";
-    divShowTime.style.height = "50px";
-    divShowTime.style.margin = "10px";
-    divShowTime.setAttribute("id", "showTime".concat(index));
-    /////////////////////////////////////////////////////////////////////
-    var startBtn = document.createElement("button");
-    startBtn.innerText = "Start";
-    startBtn.setAttribute("id", "startBtn".concat(index));
-    startBtn.addEventListener("click", list[index].start);
-    var stopBtn = document.createElement("button");
-    stopBtn.innerText = "Stop";
-    stopBtn.setAttribute("id", "stopBtn".concat(index));
-    stopBtn.addEventListener("click", list[index].stop);
-    var resetBtn = document.createElement("button");
-    resetBtn.innerText = "Reset";
-    resetBtn.setAttribute("id", "resetBtn".concat(index));
-    resetBtn.addEventListener("click", list[index].reset);
-    /////////////////////////////////////////////////////////////////////
-    divAnyCornometer.appendChild(divShowTime);
-    divAnyCornometer.appendChild(startBtn);
-    divAnyCornometer.appendChild(stopBtn);
-    divAnyCornometer.appendChild(resetBtn);
-    document.body.appendChild(divAnyCornometer);
+function formatTime(duration) {
+    var minute, second, milisecond, tempDuration;
+    tempDuration = duration;
+    minute = Math.floor(tempDuration / 60000);
+    tempDuration %= 60000;
+    second = Math.floor(tempDuration / 1000);
+    milisecond = tempDuration % 1000;
+    var finalMinute, finalSecond, finalMilisecond;
+    if (Math.floor(minute / 10) == 0) {
+        finalMinute = "0";
+        finalMinute += minute.toString();
+    }
+    else
+        finalMinute = minute.toString();
+    if (Math.floor(second / 10) == 0) {
+        finalSecond = "0";
+        finalSecond += second.toString();
+    }
+    else
+        finalSecond = second.toString();
+    finalMilisecond = milisecond.toString();
+    var finalDuration = finalMinute + ":" + finalSecond + "." + finalMilisecond;
+    return finalDuration;
 }
+var time;
 (_a = document.getElementById("addBtn")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
-    var c = new cornometer();
-    listofObjects.push(c);
-    creatCornometerDOM(listofObjects, objectNumber);
-    objectNumber++;
+    new cornometer();
 });
-var objectNumber = 0;
-var listofObjects = new Array;
+// document.getElementById("addBtn")?.addEventListener("click", () => {
+//     const c = new cornometer();
+//     listofObjects.push(c);
+//     creatCornometerDOM(listofObjects, objectNumber);
+//     objectNumber++;
+// })
+// let objectNumber: number = 0;
+// let listofObjects: Array<cornometer> = new Array<cornometer>;
--- cornometer.ts
+++ cornometer.ts
@@ -2,101 +2,115 @@ class cornometer {
     private _currentTime: number;
     private _duration: number;
     private _status: string;
+    private _intervalID : number
     constructor() {
         this._duration = 0;
         this._status = "stopped";
+        this.render();
     }
     public start() {
         if (this._status == "started")
             throw new Error("The cornometer already started");
-        this._currentTime = Date.now();
-        this._status = "started";
+        else {
+            this._currentTime = Date.now();
+            this._status = "started";
+        }
     }
     public stop() {
         if (this._status == "stopped")
         throw new Error("The cornometer already stopped");
-        //this._duration+=1;
-        this._duration = this._currentTime + this._duration;
-        this.show();
-        this._status = "stopped";
-    }
-    // public checkStatus() : string {
-    //     if (this._status == "started")
-    //     {
-    //         this._duration +=1; //= this._duration + this._currentTime;
-    //         //this._currentTime = Date.now();
-    //     }
-    //     return this.show();
-    // }
-    public show(){
-        let minute, second, milisecond, tempDuration: number;
-        tempDuration = this._duration;
-        minute = Math.floor(tempDuration / 60000);
-        tempDuration %= 60000;
-        second = Math.floor(tempDuration / 1000);
-        milisecond = tempDuration % 1000;
-        let finalMinute, finalSecond, finalMilisecond: string;
-        if (Math.floor(minute / 10) == 0) {
-            finalMinute = "0";
-            finalMinute += minute.toString();
+        else {
+            clearInterval(this._intervalID);
+            this._duration = (Date.now() - this._currentTime) + this._duration;
+            this._status = "stopped";
         }
-        else
-            finalMinute = minute.toString();
-        if (Math.floor(second / 10) == 0) {
-            finalSecond = "0";
-            finalSecond += second.toString();
-        }
-        else
-            finalSecond = second.toString();
-        finalMilisecond = milisecond.toString()
-        const finalDuration = finalMinute + ":" + finalSecond + "." + finalMilisecond;
-        console.log(finalDuration);
-        
-        //return finalDuration;
     }
     public reset() {
         if (this._status == "started")
             this.stop();
         this._duration = 0;
     }
+    render() {
+        const divAnyCornometer = document.createElement("div");
+        divAnyCornometer.style.border = "2px solid black";
+        divAnyCornometer.style.width = "300px";
+        divAnyCornometer.style.height = "100px";
+        //////////////////////////////////////////////////////////////////////
+        const divShowTime = document.createElement("div");
+        divShowTime.style.border = "1px solid black";
+        divShowTime.style.width = "200px";
+        divShowTime.style.height = "50px";
+        divShowTime.style.margin = "10px";
+        //divShowTime.setAttribute("id", `showTime${index}`);
+        /////////////////////////////////////////////////////////////////////
+        const startBtn = document.createElement("button");
+        startBtn.innerText = "Start";
+
+        startBtn.addEventListener("click", () => {
+            this.start();
+            this._intervalID = setInterval(() => {
+                divShowTime.innerHTML = formatTime((Date.now() - this._currentTime) + this._duration);
+            }, 10)
+        });
+        const stopBtn = document.createElement("button");
+        stopBtn.innerText = "Stop";
+
+        stopBtn.addEventListener("click", () => {
+            this.stop();
+            //clearInterval(time);
+            //time = 0;
+        });
+        const resetBtn = document.createElement("button");
+        resetBtn.innerText = "Reset";
+
+        resetBtn.addEventListener("click", () => {
+            this.reset();
+            divShowTime.innerHTML = formatTime(this._duration);
+        });
+        /////////////////////////////////////////////////////////////////////
+        divAnyCornometer.appendChild(divShowTime);
+        divAnyCornometer.appendChild(startBtn);
+        divAnyCornometer.appendChild(stopBtn);
+        divAnyCornometer.appendChild(resetBtn);
+        document.body.appendChild(divAnyCornometer);
+
+    }
+
+   
 }
-function creatCornometerDOM(list : Array<cornometer>, index: number): void {
-    const divAnyCornometer = document.createElement("div");
-    divAnyCornometer.style.border = "2px solid black";
-    divAnyCornometer.style.width = "300px";
-    divAnyCornometer.style.height = "100px";
-    //////////////////////////////////////////////////////////////////////
-    const divShowTime = document.createElement("div");
-    divShowTime.style.border = "1px solid black";
-    divShowTime.style.width = "200px";
-    divShowTime.style.height = "50px";
-    divShowTime.style.margin = "10px";
-    divShowTime.setAttribute("id", `showTime${index}`);
-    /////////////////////////////////////////////////////////////////////
-    const startBtn = document.createElement("button");
-    startBtn.innerText = "Start";
-    startBtn.setAttribute("id", `startBtn${index}`);
-    startBtn.addEventListener("click", list[index].start);
-    const stopBtn = document.createElement("button");
-    stopBtn.innerText = "Stop";
-    stopBtn.setAttribute("id", `stopBtn${index}`);
-    stopBtn.addEventListener("click", list[index].stop);
-    const resetBtn = document.createElement("button");
-    resetBtn.innerText = "Reset";
-    resetBtn.setAttribute("id", `resetBtn${index}`);
-    resetBtn.addEventListener("click", list[index].reset);
-    /////////////////////////////////////////////////////////////////////
-    divAnyCornometer.appendChild(divShowTime);
-    divAnyCornometer.appendChild(startBtn);
-    divAnyCornometer.appendChild(stopBtn);
-    divAnyCornometer.appendChild(resetBtn);
-    document.body.appendChild(divAnyCornometer);
+function formatTime(duration: number): string {
+    let minute, second, milisecond, tempDuration: number;
+    tempDuration = duration;
+    minute = Math.floor(tempDuration / 60000);
+    tempDuration %= 60000;
+    second = Math.floor(tempDuration / 1000);
+    milisecond = tempDuration % 1000;
+    let finalMinute, finalSecond, finalMilisecond: string;
+    if (Math.floor(minute / 10) == 0) {
+        finalMinute = "0";
+        finalMinute += minute.toString();
+    }
+    else
+        finalMinute = minute.toString();
+    if (Math.floor(second / 10) == 0) {
+        finalSecond = "0";
+        finalSecond += second.toString();
+    }
+    else
+        finalSecond = second.toString();
+    finalMilisecond = milisecond.toString()
+    const finalDuration = finalMinute + ":" + finalSecond + "." + finalMilisecond;
+    return finalDuration;
 }
+let time :number;
 document.getElementById("addBtn")?.addEventListener("click", () => {
-    const c = new cornometer();
-    listofObjects.push(c);
-    creatCornometerDOM(listofObjects, objectNumber);
-    objectNumber++;
+    new cornometer();
 })
-let objectNumber: number = 0;
-let listofObjects: Array<cornometer> = new Array<cornometer>;
+// document.getElementById("addBtn")?.addEventListener("click", () => {
+//     const c = new cornometer();
+//     listofObjects.push(c);
+//     creatCornometerDOM(listofObjects, objectNumber);
+//     objectNumber++;
+// })
+// let objectNumber: number = 0;
+// let listofObjects: Array<cornometer> = new Array<cornometer>;
